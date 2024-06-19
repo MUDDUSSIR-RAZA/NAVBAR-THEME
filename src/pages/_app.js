956f54a5c309cbc1a6e3fdf0f6a5e69871009008
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-19 13:38:24 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
+import Hero from "@/components/helper/hero";
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <ThemeProvider attribute="class">
-      <Navbar />
-      <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <Head>
+        <title>Your Website Name</title>
+      </Head>
+      <div className="min-h-screen dark:bg-[#030014] dark:overflow-y-scroll dark:overflow-x-hidden">
+        <Navbar />
+        <Hero />
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
-  </>;
+  );
 }
